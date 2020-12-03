7c6dde44502e10e648fd9debf6cce23a0d41acbc
Abderrahman Haddad <ahaddad@gesisa.net>
2020-12-03 06:02:02 +0100

    fix

--- Commerce.Amazon.Web/Program.cs
+++ Commerce.Amazon.Web/Program.cs
@@ -3,16 +3,17 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace Commerce.Amazon.Web
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			CreateWebHostBuilder(args).Build().Run();
-		}
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
 
-		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-			WebHost.CreateDefaultBuilder(args)
-				.UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseUrls("http://0.0.0.0:80")
+                .UseStartup<Startup>();
 
-	}
+    }
 }
